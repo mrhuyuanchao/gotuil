ff4dbabb95ffc6f09345162b7472cd822fe9d8de
呼源朝 <mryuanchao@huyuanchaodeMBP.lan>
2019-01-27 11:25:33 +0800

    修改数据库初始化

--- database.go
+++ database.go
@@ -1,19 +1,17 @@
 package goutil
 
 import (
+	"fmt"
+
 	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/xormplus/xorm"
 )
 
 // InitDB 初始化数据库配置
-func InitDB(config *DBSetting) *xorm.Engine {
+func InitDB(config *DBSetting) (*xorm.Engine, error) {
 	if config == nil {
-		return nil
+		return nil, fmt.Errorf("无配置")
 	}
-	engine, err := xorm.NewEngine(config.Type, config.DataSource)
-	if err != nil {
-		LogClient.Error(err.Error())
-	}
-	return engine
+	return xorm.NewEngine(config.Type, config.DataSource)
 }
